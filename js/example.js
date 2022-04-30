d2eca8f5fb4f618809880bf77fdf4573e01145a1
Leon3854 <frad109@mail.ru>
2022-05-01 00:41:49 +0500

    finish work path 3

---
+++ js/example.js
@@ -0,0 +1,15 @@
+/*jslint node: true */
+"use strict";
+
+let num = 10;
+
+function showFirstMessage(text) {
+    console.log(text);
+}
+
+showFirstMessage("Hello Worold!");
+
+function calc(a, b) {
+    return(a + b);
+}
+console.log(calc(4, 3));
