4c83d0c41579d5fa65f79ad5f59abd5ce0657455
Leon3854 <frad109@mail.ru>
2022-05-05 00:08:08 +0500

    Starting the working to Path-4 oure project

--- js/example.js
+++ js/example.js
@@ -1,15 +1,17 @@
 /*jslint node: true */
 "use strict";
 
-let num = 10;
 
-function showFirstMessage(text) {
-    console.log(text);
-}
 
-showFirstMessage("Hello Worold!");
 
-function calc(a, b) {
-    return(a + b);
-}
-console.log(calc(4, 3));
+	let arr = ['q', 'w','e','r','t','y','u','i','o','p'];
+	arr.forEach(function(item, i, arr) {
+		console.log(`${i}: ${item} внутри данного массива ${arr}`);
+	});
+	//console.log(arr.length);
+	// for (let i = 0; i <= arr.length; i++) {
+	// 	console.log(arr[i]);
+	// }
+
+
+	
